670deef92062bae23ab4d7253680dd15853f8c13
AlexAksioma <alexmedqwerty@gmail.com>
2024-05-06 19:39:20 +0300

    first

--- src/main/java/SQLUsers.java
+++ src/main/java/SQLUsers.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class SQLUsers {
+import java.sql.*;
+import java.util.Random;
+
+public class SQLUsers {
+    public static final String URL = "jdbc:mysql://localhost:3306/users";
+    public static final String USER_NAME = "root";
+    public static final String PASSWORD = "root";
+
+    static Connection connection;
+    static Statement statement;
+
+    public static void main(String[] args) {
+        try {
+            connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+            //connection.close();
+            if (!connection.isClosed())
+                System.out.println("connection created");
+            else
+                System.out.println("connection closed");
+            int i = new Random().nextInt(1000) + 1000;
+            statement = connection.createStatement();
+            statement.executeUpdate("insert  into user_table (id, email, password) " +
+                    "values (" + i + ", 'new_added1@mail.com', 'added" + i + "');");
+            ResultSet resultSet = statement.executeQuery("select * from user_table;");
+            while (resultSet.next()) {
+                System.out.print(resultSet.getString("id") + "  ");
+                System.out.print(resultSet.getString(2) + "  ");
+                System.out.println(resultSet.getString(3) + "  ");
+            }
+            System.out.println("================================================");
+            if( statement.execute("select * from user_table where id=1071;")){
+                resultSet = statement.getResultSet();
+                while (resultSet.next()) {
+                    System.out.print(resultSet.getString("id") + "  ");
+                    System.out.print(resultSet.getString(2) + "  ");
+                    System.out.println(resultSet.getString(3) + "  ");
+                }
+            }
+
+            connection.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
---
+++ src/main/resources/users.sql
@@ -0,0 +1,2 @@
+select * from user_table where id=200;
+insert  into user_table (id, email, password) values (500, 'added@mail.com', 'added');
